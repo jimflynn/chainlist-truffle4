e72fa2b01984a01e7d9640fbe13b67702f988c10
Jim Flynn <jim.flynn@overlook.tv>
2019-06-19 12:49:17 -0400

    first commit

---
+++ contracts/Ownable.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.18;
+
+contract Ownable {
+  address owner;
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  function Ownable() public {
+    owner = msg.sender;
+  }
+}
